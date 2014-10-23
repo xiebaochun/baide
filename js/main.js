b538d38c37ea0a7c8e87caf1f378550694de2ea7
xiebaochun <463449055@qq.com>
2014-10-23 12:54:11 +0800

    update

--- js/main.js
+++ js/main.js
@@ -12,11 +12,56 @@ $(function () {
  	                   "top":"80%"});
 
   $("#start_bt").click(function(){
+  	  console.log("start click");
   	  $("#bg").attr("src","images/start-sheet0.png");
   	  $("#start_bt").hide();
-  	  setInterval(function(){
+  	  setTimeout(function(){
   	  	$("#bg").attr("src","images/beijing2-sheet0.png");
+  	  	$("#img_left").attr("src","images/tu1_left-sheet0.png");
+  	  	$("#img_right").attr("src","images/tu1_right-sheet0.png");
+
+        $("#img_left_next").attr("src","images/tu2_left-sheet0.png");
+  	  	$("#img_right_next").attr("src","images/tu2_right-sheet0.png");
+
+  	  	$(".game_img").show();
+  	  	$("#img_right").on("swipe",function(){
+
+
+
+
+		  $("#img_left").animate({
+			opacity: 0.5,
+			left: "-=100",
+			top: "+=200"
+			}, 1000, function() {
+			// Animation complete.
+			$(this).css({"left":"10%","top":"10%","z-index":"0"});
+			$("#img_left_next").css("z-index","1");
+  	  	    $("#img_right_next").css("z-index","1");
+			});
+
+  	  	  $("#img_right").animate({
+		    opacity: 0.5,
+		    left: "+=100",
+		    top: "+=200"
+		  }, 1000, function() {
+		    $(this).css({"left":"10%","top":"10%","z-index":"0"});
+		  });
+
+
+
+
+
+
+		});
+  	  	setInterval(playGame,1000/60);
   	  },1000);
   });
+
+  function playGame(){
+  	console.log("update");
+    
+
+  }
 
 });
