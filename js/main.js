95aa34c14314b3461f881d2294d7d8343f78dec6
xiebaochun <463449055@qq.com>
2014-10-23 14:42:55 +0800

    update

--- js/main.js
+++ js/main.js
@@ -1,7 +1,13 @@
 $(function () {
    // alert(jQuery(window).height());
+   var index=2;
+isStart=false;
+var time=0;
+var count=0;
+var isSwiping=false;
+var timeCallBack;
  $("#bg").attr("src","images/beijing-sheet0.png");
-
+//$("#bg").attr("src","images/beijing2-sheet0.png");
  $("#bg").css({"height":jQuery(window).height()+"px","width":"100%"});
 
  $("#start_bt").attr("src","images/tiaozhan-sheet0.png");
@@ -17,6 +23,8 @@ $(function () {
   	  $("#bg").attr("src","images/start-sheet0.png");
   	  $("#start_bt").hide();
   	  setTimeout(function(){
+  	  	$("#count").show();
+  	  	$("#time").show();
   	  	$("#bg").attr("src","images/beijing2-sheet0.png");
   	  	$("#img_left").attr("src","images/tu1_left-sheet0.png");
   	  	$("#img_right").attr("src","images/tu1_right-sheet0.png");
@@ -26,51 +34,98 @@ $(function () {
 
   	  	$(".game_img").show();
 
-  	  	$(".game_img").swipe({
+  	  	$("#img_right").swipe({
 		  swipe:function(event, direction, distance, duration, fingerCount) {
+		  	if(isSwiping)return;
+		  	isSwiping=true;
 		    console.log("You swiped " + direction );
+		    index+=1;
+            count++;
+            $("#count_number").html(count);
+		    if(!isStart){
+		    	isStart=true;
+		    	timeCallBack=setInterval(playGame,2000);
+		    }
 		    if(direction=="right"){
 			    	$("#img_left").animate({
 					
 					left: "-=50%",
-					top: "30%"
-					}, 2000, function() {
+					top: "60%"
+					}, 5000, function() {
 					// Animation complete.
 					$(this).css({"left":"15%","top":"20%","z-index":"0"});
+                    $(this).attr("src","images/tu"+index+"_left-sheet0.png");
 					$("#img_left_next").css("z-index","1");
 		  	  	    $("#img_right_next").css("z-index","1");
+		  	
+                     isSwiping=false;
 					});
 
 		  	  	  $("#img_right").animate({
 				    
 				    left: "+=50%",
-				    top: "30%"
-				  }, 2000, function() {
+				    top: "60%"
+				  }, 5000, function() {
 				    $(this).css({"left":"15%","top":"20%","z-index":"0"});
+				    $(this).attr("src","images/tu"+index+"_right-sheet0.png");
 				  });
 		    }
 		  }
 		});
-  	  	$("#img_right").on("swipe",function(){
-      
 
+		$("#img_right_next").swipe({
+		  swipe:function(event, direction, distance, duration, fingerCount) {
+		  	if(isSwiping)return;
+		  	isSwiping=true;
+		    console.log("You swiped " + direction );
 
+		    index+=1;
+		    count++;
+            $("#count_number").html(count);
+		    
+		    if(direction=="right"){
+			    	$("#img_left_next").animate({
+					
+					left: "-=50%",
+					top: "60%"
+					}, 5000, function() {
+					// Animation complete.
+					$(this).css({"left":"15%","top":"20%","z-index":"0"});
+					$(this).attr("src","images/tu"+index+"_left-sheet0.png");
+					$("#img_left").css("z-index","1");
+		  	  	    $("#img_right").css("z-index","1");
+		  	
+                    isSwiping=false;
+					});
 
-		  
-
-
-
-
-
-
+		  	  	  $("#img_right_next").animate({
+				    
+				    left: "+=50%",
+				    top: "60%"
+				  }, 5000, function() {
+				    $(this).css({"left":"15%","top":"20%","z-index":"0"});
+				    $(this).attr("src","images/tu"+index+"_right-sheet0.png");
+				  });
+		    }
+		  }
 		});
-  	  	// setInterval(playGame,1000/60);
+  	  	
+  	  	
   	  },1000);
   });
 
   function playGame(){
-  	console.log("update");
-    
+  	console.log("click");
+  	time++;
+  	$("#time_number").html(time);
+    if(time>=8){
+        clearInterval(timeCallBack);
+    	$("#count").hide();
+  	  	$("#time").hide();
+  	  	$(".game_img").hide();
+  	  	
+    	$("#bg").attr("src","images/beijing3-sheet0.png");
+    }
 
   }
 
